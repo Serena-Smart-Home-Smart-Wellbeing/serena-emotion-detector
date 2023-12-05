d23ebf5fd6e8012eae993965d2446c9958104acd
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2023-12-05 09:41:24 +0700

    Implement transform_image and predict

--- deploy/main.py
+++ deploy/main.py
@@ -1,35 +1,61 @@
-import os
 import io
-from tensorflow import keras
+import os
+
+import numpy as np
+from flask import Flask, jsonify, request
 from PIL import Image
-from flask import Flask, request, jsonify
+from tensorflow import keras
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-model = keras.models.load_model(
-    "./model/models_model_CNN_facial_emotion_detector.h5", compile=False
-)
+model = keras.models.load_model("./model/serena-emotion-detector.h5", compile=False)
 model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+labels = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
 
 def transform_image(image):
     # TODO: buat fungsi untuk transformasi gambar sesuai dengan yang dilakukan di training untuk bisa digunakan dalam model.predict
-    pass
+    # Turn to grayscale
+    transformed_image = image.convert("L")
+
+    # This one causes every class to be around 14% predicted
+    # transformed_image = np.asarray(transformed_image)
+    # transformed_image = transformed_image / 255
+    # transformed_image = transformed_image[np.newaxis, ..., np.newaxis]
+    # transformed_image = tf.image.resize(transformed_image, [48, 48])
+
+    # Resize to 48x48
+    transformed_image = transformed_image.resize((48, 48))
+    # Reshape to np array
+    transformed_image = np.reshape(transformed_image, (1, 48, 48, 1))
+    return transformed_image
+
+
+# Function to retrieve the label corresponding to an emotion
+def get_emotion_label(emotion_code):
+    # Emotion labels mapping
+    emotion_labels = {
+        0: "Angry",
+        1: "Disgust",
+        2: "Fear",
+        3: "Happy",
+        4: "Sad",
+        5: "Surprise",
+        6: "Neutral",
+    }
+    return emotion_labels.get(emotion_code, "Invalid emotion")
 
 
 def predict(image):
     # TODO: predict menggunakan model yang sudah diload di atas dan return hasil prediksi berupa desimal dari 7 emosi + labelnya dalam bentuk Dictionary
+    # Predict
+    prediction = model.predict(image)
+    # Get labels
+    result = {}
+    for i, label in enumerate(labels):
+        percentage = prediction[0][i] * 100
+        result[label.lower()] = percentage
 
-    result = {
-        # TODO Ubah nilai-nilai di bawah ini dengan hasil prediksi dari model
-        "angry": 0,
-        "fear": 0,
-        "surprise": 0,
-        "disgust": 0,
-        "happy": 0,
-        "neutral": 0,
-        "sad": 0,
-    }
     return result
 
 
@@ -46,8 +72,8 @@ def index():
         try:
             image_bytes = file.read()
             user_photo = Image.open(io.BytesIO(image_bytes))
-            tensor = transform_image(user_photo)
-            prediction = predict(tensor)
+            transformed_image = transform_image(user_photo)
+            prediction = predict(transformed_image)
             return prediction
         except Exception as e:
             return jsonify({"message": str(e), "status": 500})
--- deploy/test.sh
+++ deploy/test.sh
@@ -6,6 +6,7 @@ FLASK_PID=$!
 
 # Send the images
 for image in test/images/*; do
+  echo $image  
   curl -X POST -F "file=@$image" http://127.0.0.1:5000
 done
 
